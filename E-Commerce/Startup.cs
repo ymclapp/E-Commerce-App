dac2e9460ed11ba76b828b0ba7053f3ad3fdcfc9
ymclapp <ymclapp@yahoo.com>
2021-11-08 14:49:36 -0600

    wired up dbcontext

--- E-Commerce/Startup.cs
+++ E-Commerce/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using E_Commerce.Controllers.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,8 @@ namespace E_Commerce
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<ECommerceDbContext>(options =>
+             options.UseSqlServer(Configuration.GetConnectionString("Default Connection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
