c08bffa24c568854ed3ca749a0f17b8d02e1ee3f
ymclapp <ymclapp@yahoo.com>
2021-11-13 17:09:23 -0600

    tring to get enum to work

--- E-Commerce/Models/AdminIndexViewModel.cs
+++ E-Commerce/Models/AdminIndexViewModel.cs
@@ -18,7 +18,7 @@ namespace E_Commerce.Models
         public IList<string> ProductCategoryName { get; set; }
         public int ProductCategoryId { get; set; }
 
-        public ProductCategory productCategory { get; set; }
+        //public ProductCategory ProductCategory { get; set; }
 
     }
 }
--- E-Commerce/Models/Product.cs
+++ E-Commerce/Models/Product.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,10 +15,14 @@ namespace E_Commerce.Models
         [Required]
         [StringLength(200)]
         public string Name { get; set; }
+
+        [Column(TypeName = "money")]
         public int Price { get; set; }
         public int InventoryAmount { get; set; }
         public string Summary { get; set; }
-        public string Condition { get; set; }
+        public Enum Condition { get; set; }
+
+        [Display(Name = "Book Cover")]
         public string ProductImage { get; set; }
         public string ProductUrl { get; set; }
 
@@ -28,5 +33,15 @@ namespace E_Commerce.Models
         //Navigation Property that makes a ProductCategoryId a Foreign Key
         public ProductCategory ProductCategory { get; set; }
 
+    }
+
+    public enum Condition
+    {
+        Select,
+        New,
+        LikeNew,
+        Good,
+        Ok,
+        Poor,
     }
 }
